513dfee5065e8e00f5f57f27261ac5fdb4c33052
petimov <petr.movsesjan20@gmail.com>
2025-09-06 21:54:25 +0200

    added

--- server.js
+++ server.js
@@ -131,7 +131,7 @@ app.get("/logout", (req, res) => {
 // --- Serve React frontend in production ---
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "build")));
-    app.get("/*", (req, res) => {
+    app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "build", "index.html"));
     });
 }
